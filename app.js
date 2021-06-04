47946a1d267851a6ff96222609ec60a5fba4f0f1
2200224 <thanarat.s@cimbthai.com>
2021-06-04 16:53:17 +0700

    .

--- app.js
+++ app.js
@@ -29,20 +29,20 @@
    { urlencoded, json } = require('body-parser'),
    app = express();
  
-   const firebase = require("firebase/app")
-   require("firebase/firestore")
+ const firebase = require("firebase/app")
+ require("firebase/firestore")
  
-   var firebaseConfig = {
-     apiKey: "AIzaSyCpAxua59Wnmb3IOODt7kltAjj8kW0tqjE",
-     authDomain: "i-order-test.firebaseapp.com",
-     projectId: "i-order-test",
-     storageBucket: "i-order-test.appspot.com",
-     messagingSenderId: "942683266626",
-     appId: "1:942683266626:web:26d3220a1eebd5fa3a97db",
-     measurementId: "G-YMV4CGP7DR"
-   };
-   // Initialize Firebase
-   firebase.initializeApp(firebaseConfig);
+ var firebaseConfig = {
+   apiKey: "AIzaSyCpAxua59Wnmb3IOODt7kltAjj8kW0tqjE",
+   authDomain: "i-order-test.firebaseapp.com",
+   projectId: "i-order-test",
+   storageBucket: "i-order-test.appspot.com",
+   messagingSenderId: "942683266626",
+   appId: "1:942683266626:web:26d3220a1eebd5fa3a97db",
+   measurementId: "G-YMV4CGP7DR"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
  
  // Parse application/x-www-form-urlencoded
  app.use(urlencoded({ extended: true }));
@@ -55,23 +55,23 @@
    console.log('get!');
    let firestore = firebase.firestore()
    firestore.collection("webhookEvent").where("senderId", "==", '3741760685883256')
-   .get()
-   .then((querySnapshot) => {
+     .get()
+     .then((querySnapshot) => {
        var response = [];
        querySnapshot.forEach((doc) => {
-           // doc.data() is never undefined for query doc snapshots
-           console.log(doc.id, " => ", doc.data());
-           response.push(doc.data());
-           console.log(response.length, " === ", querySnapshot.size);
-           if(response.length === querySnapshot.size) {
-             res.send(response);
-           }
+         // doc.data() is never undefined for query doc snapshots
+         console.log(doc.id, " => ", doc.data());
+         response.push(doc.data());
+         console.log(response.length, " === ", querySnapshot.size);
+         if (response.length === querySnapshot.size) {
+           res.send(response);
+         }
        });
-   })
-   .catch((error) => {
+     })
+     .catch((error) => {
        console.log("Error getting documents: ", error);
        res.send('Error getting documents');
-   });
+     });
  
  });
  
@@ -81,17 +81,13 @@
  
    // Your verify token. Should be a random string.
    // const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-   const VERIFY_TOKEN = "TokenIOrder";
+   const VERIFY_TOKEN = "TokenOrderHub";
  
    // Parse the query params
    let mode = req.query['hub.mode'];
    let token = req.query['hub.verify_token'];
    let challenge = req.query['hub.challenge'];
  
-   console.log('mode : '+mode);
-   console.log('token : '+token);
-   console.log('challenge : '+challenge);
-   
    // Checks if a token and mode is in the query string of the request
    if (mode && token) {
  
@@ -118,13 +114,13 @@
  
    // Checks if this is an event from a page subscription
    if (body.object === 'page') {
- 
+     console.log(body);
      // Iterates over each entry - there may be multiple if batched
-     body.entry.forEach(function(entry) {
+     body.entry.forEach(function (entry) {
  
        // Gets the body of the webhook event
        let webhookEvent = entry.messaging[0];
-       console.log(webhookEvent);
+       // console.log(entry);
  
        // Get the sender ID
        let senderId = webhookEvent.sender.id;
@@ -132,26 +128,99 @@
        // Get the recipient ID
        let recipientId = webhookEvent.recipient.id;
        console.log('recipient ID: ' + recipientId);
-       if(webhookEvent.message){
-         let message = webhookEvent.message;
-         let data = { 
-             messageId: message.mid,
-             messageText: message.text,
-             senderId: senderId,
-             recipientId: recipientId,
-             timestamp: webhookEvent.timestamp
+       let ids = recipientId+"_"+senderId;
+       if (webhookEvent.message) {
+         let receivedMessage = webhookEvent.message;
+         var text = "";
+         var type = "";
+         if (receivedMessage.text) {
+           text = receivedMessage.text;
+           type = "text";
+         } else if (receivedMessage.attachments) {
+           // console.log(">>>>>>>>>>>>>>>>>>>>>");
+           // console.log(receivedMessage);
+           // console.log(receivedMessage.attachments[0]);
+           // console.log(">>>>>>>>>>>>>>>>>>>>>");
+           text = receivedMessage.attachments[0].payload.url;
+           type = receivedMessage.attachments[0].type;
+           if(receivedMessage.attachments[0].payload.sticker_id){
+             type = "sticker";
+           }
          }
+         let message = webhookEvent.message;
+         var sfDocRef = firestore.collection("webhookEvent").doc(ids);
  
-         console.log(data);
+         firestore.runTransaction((transaction) => {
+             return transaction.get(sfDocRef).then((sfDoc) => {
+               const dateObject = new Date(webhookEvent.timestamp);
+               var messageMap = { 
+                 timestamp: dateObject, 
+                 text: text, 
+                 type: type,
+                 messageId: message.mid, 
+                 epoch: webhookEvent.timestamp,
+                 owner: 'customer',
+                 isRead: 1
+               };
+                 if (!sfDoc.exists) {
+                   var messageList = [];
+                   messageList.push(messageMap);
  
-         firestore.collection("webhookEvent")
-         // .doc("test")
-         .add(data)
-         .then(function(doc){ 
-           console.info(doc) 
-         }).catch(function(error){ 
-             console.error(error) 
+                   var data = {
+                     messageId: message.mid,
+                     message: text,
+                     type: type,
+                     senderId: senderId,
+                     recipientId: recipientId,
+                     epoch: webhookEvent.timestamp,
+                     timestamp: dateObject,
+                     messageList: messageList
+                   }
+                   console.log(data);
+                   sfDocRef.set(data);
+                   return "create success";
+                 }
+                 var messageList = sfDoc.data().messageList;
+                 messageList.push(messageMap);
+ 
+                 var data = {
+                   messageId: message.mid,
+                   message: text,
+                   type: type,
+                   senderId: senderId,
+                   recipientId: recipientId,
+                   epoch: webhookEvent.timestamp,
+                   timestamp: dateObject,
+                   messageList: messageList
+                 }
+                 console.log(data);
+                 transaction.update(sfDocRef, data);
+ 
+                 return "update success";
+                 // var newPopulation = sfDoc.data().population + 1;
+                 // if (newPopulation <= 1000000) {
+                 //     transaction.update(sfDocRef, data);
+                 //     return newPopulation;
+                 // } else {
+                 //     return Promise.reject("Sorry! Population is too big.");
+                 // }
+             });
+         }).then((state) => {
+             console.log(state);
+         }).catch((err) => {
+             // This will be an "population is too big" error.
+             console.error(err);
          });
+ 
+         // firestore.collection("webhookEvent")
+         //   .doc(ids)
+         //   .set(data)
+         //   // .add(data)
+         //   .then(function (doc) {
+         //     //  console.info(doc) 
+         //   }).catch(function (error) {
+         //     console.error(error)
+         //   });
  
  
          // firestore.collection("webhookEvent")
@@ -163,7 +232,7 @@
          // });
  
        }
-       
+ 
  
        // callSendAPI(senderPsid, webhookEvent.message.text);
  
@@ -184,6 +253,16 @@
      res.sendStatus(404);
    }
  });
+ 
+ function addValue(value, aMap) {
+   aMap = aMap || [];
+   aMap.push(value);
+ }
+ 
+ function addValueToKey(key, value, aMap) {
+     aMap[key] = aMap[key] || [];
+     aMap[key].push(value);
+ }
  
  // Handles messages events
  function handleMessage(senderPsid, receivedMessage) {
@@ -254,7 +333,7 @@
    // The page access token we have generated in your app settings
    // const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
    const PAGE_ACCESS_TOKEN = "EAAQAU40wctABACRuCSlF8bZAgZC3Uk26Pjal5KIkx2zsbmgh0KLriV7tHTZBYr4WRNwgOjvdXmdM1OAHaSuqZAfOt5iDELrS8RJKpB0l3yw6VFzjAMvkywgayO81Lcbtz0Ds9P0KypluaCcYxRr0lBqZB4bDchza6ZCYW97Gndg25nWCZBaTZBtyT46FlxChp8pnSHl6KdTfFAZDZD";
-   
+ 
    const PAGE_ACCESS_TOKEN_V1 = "EAAQAU40wctABADENayzB6RyE67VixNrrGhod6ZCUJcu6DWc62mRUvla8j8Mk8gTbIKGaZBfElf6HRciuk3X2wDcgPy3ZCLrZBlHqPOYQI8qvYeTdWq9Qa6NrUF30Oh21cqt8753KHkwoYqTYTzZBnuMMC2tHaFYJmomoOZC0biRmkuUCXucW0yChmscl55ALZAQ6ZBr9hwsZB6gZDZD";
  
    // Construct the message body
@@ -296,6 +375,7 @@
  }
  
  // listen for requests :)
- var listener = app.listen(process.env.PORT || 3000, function() {
+ var listener = app.listen(process.env.PORT || 3000, function () {
    console.log('Your app is listening on port ' + listener.address().port);
  });
+ 
